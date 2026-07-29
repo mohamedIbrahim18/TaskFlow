5482549cdcceb7602987620d493f26a51efd15c2
Mohamed Ibrahim <mohamedibrahemofficial@gmail.com>
2026-07-29 18:40:41 +0300

    Initial Commit

---
+++ TaskFlow/TaskFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskFlowApp.swift
+//  TaskFlow
+//
+//  Created by Mohamed Ibrahim on 29/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct TaskFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
